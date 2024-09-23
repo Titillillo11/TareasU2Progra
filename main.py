e1897ba9496bfd78fdb0a9b13728215c4a909beb
Jorge Luis Chávez Peñaloza <l22121045@morelia.tecnm.mx>
2024-09-22 23:41:08 -0600

    Se hizo la tarea 5

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-from coche import Coche
+from circulo import Circulo
 
 def main():
-    coche1 = Coche("Toyota", "Corolla", 2015)
-    coche2 = Coche("Ford", "Mustang", 2018)
-    coche3 = Coche("Chevrolet", "Spark", 2020)
+    circulo1 = Circulo(5)
+    circulo2 = Circulo(10)
+    circulo3 = Circulo(15)
 
-    coche1.mostrar_informacion()
-    coche2.mostrar_informacion()
-    coche3.mostrar_informacion()
-
-    año_actual = 2024
-    print(f"El {coche1.marca} {coche1.modelo} tiene {coche1.calcular_edad_del_coche(año_actual)} años.")
-    print(f"El {coche2.marca} {coche2.modelo} tiene {coche2.calcular_edad_del_coche(año_actual)} años.")
-    print(f"El {coche3.marca} {coche3.modelo} tiene {coche3.calcular_edad_del_coche(año_actual)} años.")
+    print(f"Círculo 1 (Radio: {circulo1.radio}) - Área: {circulo1.calcular_area():.2f}, Perímetro: {circulo1.calcular_perimetro():.2f}")
+    print(f"Círculo 2 (Radio: {circulo2.radio}) - Área: {circulo2.calcular_area():.2f}, Perímetro: {circulo2.calcular_perimetro():.2f}")
+    print(f"Círculo 3 (Radio: {circulo3.radio}) - Área: {circulo3.calcular_area():.2f}, Perímetro: {circulo3.calcular_perimetro():.2f}")
 
 if __name__ == "__main__":
     main()
